31576a0a3ca1db79149c1cbe3e0673e68d84be31
Vedant <khapekarvedant@gmail.com>
2024-09-20 23:22:46 +0530

    All files added

---
+++ Home.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(page_title="Real Estate App")
+st.title("The Real Estate App")
+
+st.markdown("This Real Estate App, a machine learning-based platform offers comprehensive insights and tools for property analysis, price prediction, and recommendations. This project aims to provide users with an intuitive interface to make informed decisions in the real estate market.")
+st.divider()
+st.subheader("Key Features:")
+st.markdown("1. Analytics Module - Explore detailed data insights on properties, including flats, houses, and sectors. Analyze trends and gain valuable information about different property types and locations. This module helps users understand market trends, average prices, and key factors affecting real estate in different areas.")
+st.markdown("2. Price Predictor Module: Our price prediction tool allows users to estimate the price of a property based on various features such as location, size, amenities, and more. Simply enter the property details, and our machine learning model will provide an accurate price estimate, helping you make data-driven decisions.")
+st.markdown("3. Recommender Module: Discover similar properties based on price, location advantages, and nearby amenities. Our recommendation engine uses advanced algorithms to suggest properties that meet your preferences, helping you find the perfect property quickly and efficiently.")
+st.divider()
+st.subheader('How It Works:')
+st.markdown("1. Navigate through an easy-to-use web interface built with Streamlit, designed for a seamless user experience.")
+st.markdown("2. Access detailed property analytics for flats, houses, and sectors to stay informed about the real estate market.")
+st.markdown("3. Use the Price Predictor to check the estimated value of any property by providing essential details.")
+st.markdown("4. Get personalized recommendations for properties based on factors like price, location benefits, and proximity to key areas.")
+st.divider()
+st.write(">> Whether you're a buyer, seller, or investor, the Real Estate Capstone Project is designed to empower you with the right tools and insights to make smart real estate decisions.")
